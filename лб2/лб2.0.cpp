7748c83ab67a2a0481bb46a7a3a995778a88055e
unknown <qwisquirrel@gmail.com>
2024-10-25 12:23:40 +0300

    first brance_02 commit

--- лб2/лб2.0.cpp
+++ лб2/лб2.0.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-    double a, z1,
-       // double z2;
+    double a,// z1,
+        double z2;
 
     cout << " a: ";
     cin >> a;
 
-    z1 = ((a + 2) / sqrt(2 * a) - a / sqrt(2 * a + 2) + 2 / (a - sqrt(2 * a))) * (sqrt(a) - sqrt(2)) / (a + 2);
-   // z2 = 1 / sqrt(a + sqrt(2));
-    cout << "z1 = " << z1 << endl;
-   // cout << "z2 = " << z2 << endl;
+    //z1 = ((a + 2) / sqrt(2 * a) - a / sqrt(2 * a + 2) + 2 / (a - sqrt(2 * a))) * (sqrt(a) - sqrt(2)) / (a + 2);
+    z2 = 1 / sqrt(a + sqrt(2));
+    //cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 
     return 0;
 }
